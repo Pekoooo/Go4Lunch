8b8cc694a89f9fffb664a8b986753ac79760405d
Pekoooo <89356807+Pekoooo@users.noreply.github.com>
2022-03-13 09:58:37 -0700

    Fixed all fragments showing google map Added a method that checks if the user's device has the good version

--- app/src/main/java/com/example/go4lunch/MainActivity.java
+++ app/src/main/java/com/example/go4lunch/MainActivity.java
@@ -11,36 +11,69 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.karumi.dexter.listener.single.DialogOnDeniedPermissionListener;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
     private BottomNavigationView bottomNavigationView;
     private NavController navController;
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //init fragment
+        if (isServiceOK()) {
+            init();
+        }
+    }
 
-        Fragment fragment = new MapFragment();
+    // Checks if user's device has the good version
+    private boolean isServiceOK() {
+        Log.d(TAG, "isServiceOK: checking google services version");
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if (available == ConnectionResult.SUCCESS) {
+            //Everything is fine and the user can make map queries
+            Log.d(TAG, "isServiceOK: Google Play Services is working");
+            return true;
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+            //an error occurred but we can resolve it
+            Log.d(TAG, "isServiceOK: a fixable error has been detected");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "You cannot make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    private void init() {
+        //init fragment
+        Fragment fragment = new Fragment();
 
         //Open Fragment
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.fragment,fragment)
+                .replace(R.id.fragment, fragment)
                 .commit();
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_view);
-        NavController navController = Navigation.findNavController(this,  R.id.fragment);
-        NavigationUI.setupWithNavController(bottomNavigationView, navController);;
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -50,16 +83,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        //Toggle the hamburger icon to open and close the drawer menu
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
     }
 
     @Override
     public void onBackPressed() {
-        if(drawer.isDrawerOpen(GravityCompat.START)){
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
@@ -68,7 +101,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.nav_your_lunch:
                 Intent intent = new Intent(MainActivity.this, YourLunchActivity.class);
                 startActivity(intent);
--- app/src/main/java/com/example/go4lunch/MapFragment.java
+++ app/src/main/java/com/example/go4lunch/MapFragment.java
@@ -1,10 +1,15 @@
 package com.example.go4lunch;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -17,18 +22,26 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
     LocationManager locationManager;
     LocationListener locationListener;
     LatLng userLatLong;
 
-
+    private GoogleMap gMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Initialize view
         View view = inflater.inflate(R.layout.fragment_map, container, false);
 
@@ -37,10 +50,25 @@ public class MapFragment extends Fragment {
                 getChildFragmentManager().findFragmentById(R.id.google_map);
 
         //Async map
-        if(supportMapFragment != null) {
+        if (supportMapFragment != null) {
             supportMapFragment.getMapAsync(new OnMapReadyCallback() {
                 @Override
                 public void onMapReady(@NonNull GoogleMap googleMap) {
+                    gMap = googleMap;
+
+                    locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+                    locationListener = new LocationListener() {
+                        @Override
+                        public void onLocationChanged(@NonNull Location location) {
+                            userLatLong = new LatLng(location.getLatitude(), location.getLongitude());
+                            gMap.clear();
+                            gMap.addMarker(new MarkerOptions().position(userLatLong));
+                            gMap.moveCamera(CameraUpdateFactory.newLatLng(userLatLong));
+
+                        }
+                    };
+
+
                     //When map is loaded
                     googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
                         @Override
@@ -54,19 +82,24 @@ public class MapFragment extends Fragment {
                             //Set title of marker
                             markerOptions.title(latLng.latitude + " : " + latLng.longitude);
                             //Remove all marker
-                            googleMap.clear();
-                            //Animating to soom the marker
-                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                            gMap.clear();
+                            //Animating to zoom the marker
+                            gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
                                     latLng, 10
                             ));
                             //Add marker on map
-                            googleMap.addMarker(markerOptions);
+                            gMap.addMarker(markerOptions);
                         }
                     });
-
                 }
             });
         }
         return view;
+    }
+
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
     }
 }
